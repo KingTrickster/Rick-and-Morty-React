b349dc03863f3a69010943e511d793abd0ef13e3
Alejandro Trejo <alextrejores@gmail.com>
2023-01-29 18:11:14 -0600

    Added Components folder structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="text-center fredoka text-secondary mt-4 mb-4">Rick & Morty <span className="text-info">Wiki</span></h1>
     </div>
   );
 }
