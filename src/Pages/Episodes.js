c7aa78566a07d1dbfe25ae956f552ce945999aac
Alejandro Trejo <alextrejores@gmail.com>
2023-03-04 00:28:58 -0600

    Completed Episodes page

--- src/Pages/Episodes.js
+++ src/Pages/Episodes.js
@@ -1,7 +1,53 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Cards from "../components/Cards/Cards";
+import InputGroup from "../components/Filters/Category/InputGroup";
 
 const Episodes = () => {
-  return <div>The Episodes are here</div>;
+  let [episodeId, setEpisodeId] = useState(1);
+  let [info, setInfo] = useState([]);
+  let [results, setResults] = useState([]);
+  let { count, air_date, name } = info;
+  let api = `https://rickandmortyapi.com/api/episode/${episodeId}`;
+
+  useEffect(() => {
+    (async function () {
+      let data = await fetch(api).then((res) => res.json());
+      setInfo(data);
+
+      let a = await Promise.all(
+        data.characters.map((x) => {
+          return fetch(x).then((res) => res.json());
+        })
+      );
+      setResults(a);
+    })();
+  }, [api]);
+
+  return (
+    <div className="container">
+      <div className="row mb-5">
+        <h1 className="text-center mb-4">
+          Episode :{" "}
+          <span className="text-info ">{name === "" ? "Unknown" : name}</span>
+        </h1>
+        <h5 className="text-center">
+          {" "}
+          Air Date: {air_date === "" ? "Unknown" : air_date}{" "}
+        </h5>
+      </div>
+      <div className="row">
+        <div className="col-3">
+          <h4 className="text-center mb-4">Pick Episode</h4>
+          <InputGroup setEpisodeId={setEpisodeId} name="Episode" total={51} />
+        </div>
+        <div className="col-8">
+          <div className="row">
+            <Cards results={results} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Episodes;
